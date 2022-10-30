894ae09845f19b594b5d49fef168afa261bbe3c4
ElinorBe <49240498+ElinorBe@users.noreply.github.com>
2022-10-30 11:51:57 +0200

    upload

---
+++ PhoneBook.java
@@ -0,0 +1,171 @@
+package JavaTest2;
+
+import java.io.*;
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class PhoneBook {
+    ArrayList<Contact> PhoneBook;
+    ArrayList<Contact> temp;
+    boolean flag;
+
+    PhoneBook() {
+        PhoneBook = new ArrayList<>();
+    }
+
+    public void AddContact(Contact c) {
+        PhoneBook.add(c);
+        System.out.println("Contact added successfully!\n\n\n");
+        temp = PhoneBook; /* temp gets phonebook for future changes - we don't want that the changes will be on the original arrayList (PhoneBook) */
+    }
+
+    /* function gets a name from the user -
+    Into the loop I'm checking if the name is equal to contact name - if it is, the contact will be removed and a message will be shown. */
+    public void DeleteContact_ByName(String name) {
+        flag = false; /* The boolean variable is to know if there was at least 1 user that removed.
+        If so, then the flag gets true, if not the flag stay false and that's how I know that no contact has been removed. */
+        if (PhoneBook.size() > 0) {
+            for (int i = 0; i < PhoneBook.size(); i++)
+                if ((PhoneBook.get(i).getContactName().equals(name))) {
+                    PhoneBook.remove(i);
+                    flag = true;
+                    break;
+                }
+            if (!flag) System.out.println("Contact doesn't exist!");
+            else {
+                System.out.println("Contact deleted successfully");
+                temp = PhoneBook;
+            }
+        } else System.out.println("PhoneBook is empty!");
+    }
+
+    public void PrintPhoneBook() { // Print PhoneBook
+        if (PhoneBook.size() > 0) {
+            System.out.println("PhoneBook:");
+            for (Contact c : PhoneBook) {
+                System.out.print(c);
+            }
+        } else System.out.println("PhoneBook is empty!!");
+    }
+
+    /* function gets a name from the user -
+    Into the loop I'm checking if the name is equal to contact name - if it is, the contact will be printed. */
+    public void SearchContact_ByName(String name) { /**/
+        flag = false; // The boolean variable is to know if at least 1 user has found.
+        // If so, then the flag gets true, if not the flag stay false and that's how I know that no contact has been found.
+        if (PhoneBook.size() > 0) {
+            for (Contact c : PhoneBook)
+                if (c.getContactName().equals(name)) {
+                    System.out.println(c);
+                    flag = true;
+                }
+            if (!flag) System.out.println("The name '" + name + "' doesn't exist in the phonebook.");
+        } else System.out.println("PhoneBook is empty!!");
+    }
+
+    /* Function sorting Phonebook by ascending order (from small to large), by name, using "Collections.sort".
+     * Operations are done on temp variable in order to not change the original phonebook */
+    public void SortPhonebook_Ascending() {
+        if (temp.size() > 0) {
+            System.out.println(temp);
+            Collections.sort(temp, (c1, c2) -> c1.getContactName().compareTo(c2.getContactName())); /* Function gets the phonebook to be sorted, and c1 & c2 – the comparators to determine the order of the list.
+            The comparators are being compared each one to the next one. */
+            System.out.print(temp);
+            temp = PhoneBook;
+        } else System.out.println("PhoneBook is empty!!");
+    }
+
+    /* Function sorting Phonebook by descending order (from large to small), by name, using "Collections.sort".
+     * Operations are done on temp variable in order to not change the original phonebook */
+    public void SortPhonebook_Descending() {
+        if (temp.size() > 0) {
+            System.out.println(temp);
+            Collections.sort(temp, Collections.reverseOrder((c1, c2) -> c1.getContactName().compareTo(c2.getContactName()))); /* Function gets the phonebook to be sorted, and c1 & c2 – the comparators to determine the order of the list.
+            The comparators are being compared each one to the next one. */
+            System.out.println(temp);
+            temp = PhoneBook;
+        } else System.out.println("PhoneBook is empty!!");
+    }
+
+    /* Function organize phonebook by reverse order - from the end to the beginning.
+    Operations are done on temp variable in order to not change the original phonebook */
+    public void organizeReverse() {
+        if (temp.size() > 0) {
+            System.out.println(temp);
+            Collections.reverse(temp);
+            System.out.println(temp);
+            temp = PhoneBook;
+        } else System.out.println("PhoneBook is empty!!");
+    }
+
+    /* Function checking if there are duplicates contact - if so they will be removed.*/
+    public void DeleteDuplicateContact() {
+        if (PhoneBook.size() > 0) {
+            System.out.println("Phonebook before delete duplicate contact:\n" + PhoneBook);
+            /* In the next 2D loop the examination start with the current contact (i) compare to the next contact (j).
+             * The first loop (i) is stay with the same value until the second loop (j) gets to the end of arrayList (j < PhoneBook.size()).
+             * For each step j is growing by 1 and the comparison is being on the next contact.
+             * When j gets to the end of arrayList, the first loop (i) is growing by one and j start from the beginning again - one by one. */
+            for (int i = 0; i < PhoneBook.size(); i++)
+                for (int j = i + 1; j < PhoneBook.size(); j++)
+                    if (PhoneBook.get(i).getContactName().equals(PhoneBook.get(j).getContactName()) && PhoneBook.get(i).getContactPhoneNumber().equals(PhoneBook.get(j).getContactPhoneNumber())) {
+                        System.out.println(PhoneBook.get(i).getContactName() + PhoneBook.get(i).getContactPhoneNumber());
+                        PhoneBook.remove(j);
+                        j--; /* When a contact is being removed the arrayList is getting smaller by one, so it is necessary to decrement j by one so there is no skipping of one contact. */
+                    }
+            System.out.println(PhoneBook);
+            temp = PhoneBook;
+        } else System.out.println("PhoneBook is empty!!");
+    }
+
+    /* Phonebook is being safe to a txt file */
+    public void saveToTxtFile() throws IOException {
+        if (PhoneBook.size() > 0) {
+            File myPhoneBook = new File("PhoneBook.txt"); /*New file named "PhoneBook.txt" */
+            /* In the next try-catch I create new file with the above name, and at the same time I check if the file is already exists.
+             * If it exits or not - a relevant message will appear, and if there is some other error, an exception will be thrown. */
+            try {
+                if (myPhoneBook.createNewFile()) System.out.println("File created: " + myPhoneBook.getName());
+                else System.out.println("File already exists.");
+            } catch (IOException e) {
+                System.out.println("An error occurred.");
+                e.printStackTrace();
+            }
+            /* In the next try-catch the loop go through each line from the phonebook and write it to the file. */
+            try {
+                FileWriter wr = new FileWriter("PhoneBook.txt");
+                for (Contact c : PhoneBook)
+                    wr.write(c.getContactName() + "," + c.getContactPhoneNumber() + "\n");
+                System.out.println("Data added successfully");
+                wr.close();
+            } catch (IOException e) {
+                System.out.println("An error occurred.");
+                e.getStackTrace();
+            }
+        } else System.out.println("PhoneBook is empty!!");
+    }
+
+    public void LoadFileToPhoneBook(String path) {
+        boolean flag = false;
+        try {
+            BufferedReader bufReader = new BufferedReader(new FileReader(path)); // Path of file.
+            String strCurrentLine;
+            /* explanation on while loop:
+            "bufReader.readLine()" is reading the information line to "strCurrentLine" until it get to the end of line (null).
+            *
+            Into the loop I created a new contact, and I send to it the contact name (before ",")  and the contact phone number (after ",")
+            using split method. split method is divide the string to an array with 2 places,
+            which in this case the first place contain the information before char "," ,
+            and the second place contain the information after char "," */
+            while ((strCurrentLine = bufReader.readLine()) != null) {
+                Contact c = new Contact(strCurrentLine.split(",")[0], strCurrentLine.split(",")[1]);
+                PhoneBook.add(c);
+                flag = true;
+            }
+        } catch (Exception e) {
+            e.getStackTrace();
+        }
+        if (flag) System.out.println("Phonebook loaded successfully!");
+    }
+}
+
